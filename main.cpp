a3551d0b2e4b551f9110d56c3b9c3465eb7fa6db
Kwon-Kiseok <soorison@naver.com>
2022-04-19 10:50:38 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Timber", Style::Default);
+
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        // 입력 처리
+
+        // 업데이트
+
+        // 렌더
+
+        window.clear();
+
+        window.display();
+    }
+
+    return 0;
+}
